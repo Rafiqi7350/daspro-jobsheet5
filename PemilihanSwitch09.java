2f2b04b9984254d8732e907f4b4f0a506512c0ce
Rafiqi7350 <muhammadrasyid7350>
2024-09-24 16:09:04 +0700

    PemilihanHariDenganIf09

--- PemilihanSwitch09.java
+++ PemilihanSwitch09.java
@@ -30,7 +30,6 @@ public class PemilihanSwitch09 {
                 hasil = angka1 / angka2;
                 break;
             default:
-            System.out.println("Operator yang dipilih tidak sesuai");
             break;
         }
             System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
